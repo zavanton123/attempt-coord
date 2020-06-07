495f550c463be05a7352f340c3063cb86bdf2839
zavanton <zavanton@yandex-team.ru>
2020-06-07 13:51:52 +0300

    init

---
+++ app/src/main/java/ru/zavanton/attemptcoord/ScrollingActivity.kt
@@ -0,0 +1,14 @@
+package ru.zavanton.attemptcoord
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+        // setSupportActionBar(toolbar)
+
+    }
+}
